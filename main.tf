ed84d79b5661e5ad2fa20edcbcc173e68e13cf95
Bhanu Malhotra <144083659+bhanumalhotra123@users.noreply.github.com>
2024-04-26 03:50:46 +0530

    14 terraform cloud backend (#15)
    * #14 migrate local state to terraform cloud using terraform cloud block
    
    * Terraform Cloud Backend #14

--- main.tf
+++ main.tf
@@ -1,8 +1,22 @@
 terraform {
+  #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "ExamPro"
+
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
+  cloud {
+    organization = "Bhanumalhotra"
+    workspaces {
+      name = "terra-house-bhanuman"
+    }
+  }
   required_providers {
     random = {
       source = "hashicorp/random"
-      version = "3.6.1"
+      version = "3.5.1"
     }
     aws = {
       source = "hashicorp/aws"
@@ -11,36 +25,28 @@ terraform {
   }
 }
 
-
 provider "aws" {
-  # Configuration options
 }
-
 provider "random" {
   # Configuration options
-}
-
-resource "aws_s3_bucket" "example" {
-  # Bucket Naming Rules
-  # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
-
-  bucket = random_string.bucket_name.result
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
 }
 
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
   lower = true
   upper = false
-  length = 16
-  special = false
+  length   = 16
+  special  = false
 }
 
-
-output "random_bucket_name"{
-  value = random_string.bucket_name.result 
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+resource "aws_s3_bucket" "example" {
+  # Bucket Naming Rules
+  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
+  bucket = random_string.bucket_name.result
 }
+
+output "random_bucket_name" {
+  value = random_string.bucket_name.result
+}
+
