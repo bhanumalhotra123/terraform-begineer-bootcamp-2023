b89f6e5d23f29b2fa9a5e9b25730b2b060cf8cae
Bhanu Malhotra <bhanumalhotra219@gmail.com>
2024-05-01 22:05:56 +0000

    comment out code to see output of path.root

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,12 @@ output "s3_website_endpoint" {
   description = "S3 Static Website hosting endpoint"
   value = module.terrahouse_aws.website_endpoint
 }
+
+
+locals {
+  root_path = path.root
+}
+
+output "root_path" {
+  value = local.root_path
+}
